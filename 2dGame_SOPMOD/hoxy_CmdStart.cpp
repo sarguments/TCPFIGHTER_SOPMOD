068e86346b2336dbcc302032a8b750e055619f2b
sarguments <sarguments@gmail.com>
2017-12-13 13:39:34 +0900

    디버그 텍스트 추가

--- 2dGame_SOPMOD/hoxy_CmdStart.cpp
+++ 2dGame_SOPMOD/hoxy_CmdStart.cpp
@@ -58,6 +58,19 @@ void CCmdStart::CmdDebugText(WCHAR* text, bool bParam)
 	}
 	else
 	{
-		wcout << std::setw(30) << text << std::setw(40) << L"|ERROR| : " << std::setw(10) << GetLastError() << endl;
+		int errorNum = GetLastError();
+		wcout << std::setw(30) << text << std::setw(40) << L"|ERROR| : " << std::setw(10) << errorNum << endl;
+
+		// 파일 생성
+		FILE* fp;
+		errno_t err = _wfopen_s(&fp, L"debug.txt", L"a+t");
+		if (err != 0)
+		{
+			CCmdStart::CmdDebugText(L"file open error", false);
+			exit(1);
+		}
+
+		fwprintf(fp, L"ERROR : %d // %ws\n", errorNum, text);
+		fclose(fp);
 	}
 }
